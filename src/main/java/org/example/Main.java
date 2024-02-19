948c6c6c3341e676b9011dc34d6eccd0b9b03437
Shaikh Faiyaz Karim <190041116faiyaz@gmail.com>
2024-02-19 13:34:31 +0600

    Added images for the map and finished the overall layout.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 import processing.core.PApplet;
+import processing.core.PImage;
+
+import java.util.ArrayList;
 
 public class Main extends PApplet {
     public static void main(String[] args) {
@@ -7,19 +10,73 @@ public class Main extends PApplet {
     }
 
     final static float MOVE_SPEED = 5;
-    Sprite p;
+    final static float SPRITE_SCALE = 50.0f / 128;
+    final static float SPRITE_SIZE = 50;
 
-    public void settings() {
+    Sprite p;
+    PImage snow, crate, red_brick, brown_brick;
+    ArrayList<Sprite> platforms;
+
+    public void settings(){
         size(800, 600);
-        p = new Sprite(this, "images/ball.png", 1, 100, 300);
+    }
+
+    public void setup() {
+        imageMode(CENTER);
+        p = new Sprite(this, "images/player.png", 1.0f, 100, 300);
         p.change_x = 0;
         p.change_y = 0;
+        platforms = new ArrayList<>();
+        red_brick = loadImage("images/red_brick.png");
+        brown_brick = loadImage("images/brown_brick.png");
+        crate = loadImage("images/crate.png");
+        snow = loadImage("images/snow.png");
+        createPlatforms();
+    }
+
+    void createPlatforms(){
+        String [] lines = loadStrings("map/map.csv");
+        for(int row = 0; row < lines.length; row++){
+            String[] values = split(lines[row], ",");
+            for(int col = 0; col < values.length; col++){
+                switch (values[col]) {
+                    case "1" -> {
+                        Sprite s = new Sprite(this, red_brick, SPRITE_SCALE);
+                        s.center_x = SPRITE_SIZE / 2 + col * SPRITE_SIZE;
+                        s.center_y = SPRITE_SIZE / 2 + row * SPRITE_SIZE;
+                        platforms.add(s);
+                    }
+                    case "2" -> {
+                        Sprite s = new Sprite(this, snow, SPRITE_SCALE);
+                        s.center_x = SPRITE_SIZE / 2 + col * SPRITE_SIZE;
+                        s.center_y = SPRITE_SIZE / 2 + row * SPRITE_SIZE;
+                        platforms.add(s);
+                    }
+                    case "3" -> {
+                        Sprite s = new Sprite(this, brown_brick, SPRITE_SCALE);
+                        s.center_x = SPRITE_SIZE / 2 + col * SPRITE_SIZE;
+                        s.center_y = SPRITE_SIZE / 2 + row * SPRITE_SIZE;
+                        platforms.add(s);
+                    }
+                    case "4" -> {
+                        Sprite s = new Sprite(this, crate, SPRITE_SCALE);
+                        s.center_x = SPRITE_SIZE / 2 + col * SPRITE_SIZE;
+                        s.center_y = SPRITE_SIZE / 2 + row * SPRITE_SIZE;
+                        platforms.add(s);
+                    }
+                }
+            }
+        }
     }
 
     public void draw() {
         background(255);
         p.display();
         p.update();
+
+        for(Sprite s: platforms){
+            s.display();
+        }
     }
 
     public void keyPressed(){
@@ -51,4 +108,5 @@ public class Main extends PApplet {
             p.change_y = 0;
         }
     }
+
 }
--- src/main/java/org/example/Sprite.java
+++ src/main/java/org/example/Sprite.java
@@ -4,16 +4,16 @@ import processing.core.PApplet;
 
 public class Sprite {
     PApplet parent;
-    PImage img;
+    PImage image;
     float center_x, center_y;
     float change_x, change_y;
     float w, h;
 
     public Sprite(PApplet p, String file_name, float scale, float x, float y){
         parent = p;
-        img = parent.loadImage(file_name);
-        w = img.width * scale;
-        h = img.height * scale;
+        image = parent.loadImage(file_name);
+        w = image.width * scale;
+        h = image.height * scale;
         center_x = x;
         center_y = y;
         change_x = 0;
@@ -24,14 +24,23 @@ public class Sprite {
         this(p, file_name, scale, 0, 0);
     }
 
+    public Sprite(PApplet p, PImage img, float scale){
+        parent = p;
+        image = img;
+        w = image.width * scale;
+        h = image.height * scale;
+        center_x = 0;
+        center_y = 0;
+        change_x = 0;
+        change_y = 0;
+    }
+
     public void display(){
-        parent.image(img, center_x, center_y);
+        parent.image(image, center_x, center_y, w, h);
     }
 
     public void update(){
         center_x += change_x;
         center_y += change_y;
     }
-
-
 }
